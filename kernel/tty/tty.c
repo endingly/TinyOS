0c664f66777bccfd653dd4ba218bf9acba4d489a
endingly <471559153@qq.com>
2023-10-14 11:56:55 +0800

    一些小提交，需要解决 grub 卡住的问题

--- kernel/tty/tty.c
+++ kernel/tty/tty.c
@@ -1,12 +1,13 @@
 #include <TinyOS/tty/tty.h>
+#include <stdint-gcc.h>
 
 #define TINYOS_TTY_WIDTH 80
 #define TINYOS_TTY_HEIGHT 25
 
 vga_atrributes* buffer      = 0xB8000;
 vga_atrributes  theme_color = TINYOS_VGA_BLACK;
-int             TTY_COLUMN  = 0;
-int             TTY_ROW     = 0;
+uint32_t        TTY_COLUMN  = 0;
+uint32_t        TTY_ROW     = 0;
 
 void tty_set_color(vga_atrributes fg, vga_atrributes bg) { theme_color = (fg << 4 | bg) << 8; }
 
@@ -35,8 +36,8 @@ void tty_push_str(char* str) {
 }
 
 void tty_clear() {
-  for (int x = 0; x < TTY_COLUMN; x++) {
-    for (int y = 0; y < TTY_ROW; y++) {
+  for (uint32_t x = 0; x < TTY_COLUMN; x++) {
+    for (uint32_t y = 0; y < TTY_ROW; y++) {
       *(buffer + x + y * TINYOS_TTY_WIDTH) = (theme_color | ' ');
     }
   }
